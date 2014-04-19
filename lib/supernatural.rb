0655b917a614118e780cbd04f7bdf4b9aede23b7
Nico Saueressig <nico@nicosaueressig.de>
2014-04-20 00:17:39 +0100

    SuperNatural module established, Ruby can´t destroy objects:(!

--- lib/supernatural.rb
+++ lib/supernatural.rb
@@ -9,13 +9,13 @@ module SuperNatural
 		dimensional_rubics_cube = rand(3)
 
 		if dimensional_rubics_cube == 1
-			puts "OMG, Godzilla attacked your Airport, if you could only see, oh the HORROR...Casualty rate 100%"
+			puts "OMG, Godzilla attacked your Airport, if you could only see, oh the HORROR...Casualty rate 100%...Oh, wait Ruby saved the planes because you can´t destroyed objects in Ruby!"
 			@grounded_planes = []
 		elsif dimensional_rubics_cube == 2
-			puts "A huge tentacle grabs all planes and takes them away to another dimension. Well, that was unexpected?!"
+			puts "A huge tentacle grabs all planes and takes them away to another dimension. Well, that was unexpected?! Oh, wait Ruby saved the planes because you can´t destroyed objects in Ruby!"
 			@grounded_planes = []
 		else 
-			puts "Wormhole took your airgrounded_planes"
+			puts "Wormhole took your planes! Oh, wait Ruby saved the planes because you can´t destroyed objects in Ruby!"
 			@grounded_planes = []
 		end
 	end
